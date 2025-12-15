b5ea3ce23a06bd87fb7f8381e1771c5bfbee3a54
OseNed <92305606+OseNed@users.noreply.github.com>
2025-12-15 21:17:56 +0400

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,30 +1,33 @@
-import React, { useState } from 'react';
-import { HashRouter, Routes, Route, useLocation } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import HowItWorks from './pages/HowItWorks';
-import WhyInvest from './pages/WhyInvest';
-import HelpCenter from './pages/HelpCenter';
-import Blog from './pages/Blog';
-import Contact from './pages/Contact';
+import React from 'react'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+import Home from './pages/Home'
+import HowItWorks from './pages/HowItWorks'
+import WhyInvest from './pages/WhyInvest'
+import HelpCenter from './pages/HelpCenter'
+import Blog from './pages/Blog'
+import Contact from './pages/Contact'
 
 const ScrollToTop = () => {
-  const { pathname } = useLocation();
-  
+  const { pathname } = useLocation()
+
   React.useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-  
-  return null;
-};
+    window.scrollTo(0, 0)
+  }, [pathname])
+
+  return null
+}
 
 const App: React.FC = () => {
   return (
-    <HashRouter>
+    <>
       <ScrollToTop />
+
       <div className="flex min-h-screen flex-col font-sans">
         <Navbar />
+
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
@@ -35,10 +38,11 @@ const App: React.FC = () => {
             <Route path="/contact" element={<Contact />} />
           </Routes>
         </main>
+
         <Footer />
       </div>
-    </HashRouter>
-  );
-};
+    </>
+  )
+}
 
-export default App;
+export default App
